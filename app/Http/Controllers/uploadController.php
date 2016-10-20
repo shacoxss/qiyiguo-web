042b756664c2ed45dfb3f2b3def8e3b9a7cf47a6
363182860 <363182860@qq.com>
2016-10-20 10:41:44 +0800

    a

--- app/Http/Controllers/uploadController.php
+++ app/Http/Controllers/uploadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Users;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -15,32 +16,16 @@ class uploadController extends Controller
         $file = Input::file('avatar_file');
         if($file->isValid()){
             $extension = $file->getClientOriginalExtension();
-            $dir = base_path().'/public/upload/head_img/'.date('Ymd',time());
+            $dir = base_path().'/public/upload/userProfile/'.session('user')->id;
             if(!is_dir($dir)){
                 mkdir($dir);
             }
-
-            $string = 'ABCDEFGHIJKLMNPQRST1234567890';
-            $fileNewName = substr(str_shuffle($string),0,6).'.'.$extension;
+            $fileNewName = 'head_img'.'.'.$extension;
             $file->move($dir,$fileNewName);
-            $filepath = '/upload/head_img/'.date('Ymd',time()).'/'.$fileNewName;
-            $file_middle_path ='/upload/head_img/'.date('Ymd',time()).'/'.'middle_'.$fileNewName;
-            $file_small_path = '/upload/head_img/'.date('Ymd',time()).'/'.'small_'.$fileNewName;
-
-            //生成中小头像
-            $this->thumb($filepath,98,98,0,$file_middle_path,100);
-            $this->thumb($filepath,48,48,0,$file_small_path,100);
+            $filepath = '/upload/userProfile/'.session('user')->id.'/'.$fileNewName;
             return response()->json(['result'=>$filepath]);
         }
     }
-    //生成缩略图
-    public function thumb($oldImg,$width,$height,$cut=0,$thumbPath,$quality=100)
-    {
-        require_once ('../vendor/thumb/thumb.class.php');
-        $resize = new \ResizeImage($oldImg, $width, $height, $cut,$thumbPath,$quality);
-    }
-
-
     public function uploadWebLogo()
     {
         $file = Input::file('avatar_file');
