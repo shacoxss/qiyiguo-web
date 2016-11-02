c5cc18f74f69b774a2c1cfbab6f57bf4bbb7ff26
shacoxss <shacoxss@yeah.net>
2016-11-02 16:16:04 +0800

    author follow

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,9 @@ class AppServiceProvider extends ServiceProvider
         Tag::observe(TagObserver::class);
 
         app('view')->composer('pc_home.commonIn', function ($view) {
-            $view->with('allTags', Tag::where('status',2)->orderBy('weight','desc')->orderBy('created_at','desc')->skip(0)->take(48)->get());
+            $view->with('allTags', Tag::where('status',2)->orderBy('weight','desc')->orderBy('created_at','desc')->skip(0)->take(48)->get())
+                ->with('hot_tags',  Tag::where('status',2)->orderBy('weight','desc')->orderBy('updated_at','desc')->skip(0)->take(7)->get())
+            ;
         });
 
         app('view')->composer('inc.top-slide', 'App\Http\Controllers\Home\IndexController@topSlide');
