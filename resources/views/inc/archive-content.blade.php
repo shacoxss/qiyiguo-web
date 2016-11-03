efa2983356a6d02ad297e65880a9eed73312c65c
363182860 <363182860@qq.com>
2016-11-03 15:24:16 +0800

    a

--- resources/views/inc/archive-content.blade.php
+++ resources/views/inc/archive-content.blade.php
@@ -1,5 +1,5 @@
 <div class="news-detail-content-left-p">
-    <h2 style="color:red;">该片内容尚未通过审核，现属于预览状态！</h2>
+{{--    <h2 style="color:red;">该片内容尚未通过审核，现属于预览状态！</h2>--}}
     {!! $archive->detail->content !!}
     <p style="text-align: center;">
         @include('inc.lake-button', ['archive' => $archive])
