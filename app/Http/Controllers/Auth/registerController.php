733ca210452d216b84f56306d785b23a8d0fb255
363182860 <363182860@qq.com>
2016-11-04 15:20:38 +0800

    a

--- app/Http/Controllers/Auth/registerController.php
+++ app/Http/Controllers/Auth/registerController.php
@@ -38,7 +38,7 @@ class RegisterController extends Controller
 
     public function binding()
     {
-        return 'ok';
+        return 'error';
         if($input = Input::except('_token')){
             if($input['code'] == session('code')){
                 $user = session('user');
