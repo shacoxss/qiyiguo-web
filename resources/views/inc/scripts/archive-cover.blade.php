7a742dbe51cf755a7efbc8e450d6a3938be5a2ac
shacoxss <shacoxss@yeah.net>
2016-11-09 15:52:42 +0800

    long content cut page

--- resources/views/inc/scripts/archive-cover.blade.php
+++ resources/views/inc/scripts/archive-cover.blade.php
@@ -1,14 +1,21 @@
-<div class="form-group">
+<div class="form-group text-center">
     <p>
         @if(isset($archive) && $archive->cover)
-            <img style="max-width:250px;" src="{{route('image', [$archive->cover, '250'])}}" />
+            <img class="cover-img" src="{{route('image', [$archive->cover, '250'])}}" />
         @else
-            <img style="max-width: 250px;">
+            <img>
         @endif
     </p>
     <a href="javascript:;" class="cover btn btn-primary btn-xl"><i class="fa fa-plus"></i><span class="file-name">点击上传封面</span></a>
     <input type="file" name="cover" style="display: none;">
 </div>
+<style>
+    .cover-img {
+        max-width: 250px;
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+</style>
 <script>
     $(document).ready(function () {
         $('.cover').click(function () {
@@ -18,7 +25,7 @@
             var file = event.target.files[0];
             var box = $(this).parents('div.form-group');
 
-            box.find('img').attr('src', window.URL.createObjectURL(file))
+            box.find('img').attr('src', window.URL.createObjectURL(file)).addClass('cover-img')
 
             box.find('.file-name').text(file.name)
         })
