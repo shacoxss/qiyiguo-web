bb254d0bd27926a7dd328e4126b4629f83995c5b
shacoxss <shacoxss@yeah.net>
2016-11-11 10:54:30 +0800

    list.css

--- database/migrations/2016_10_16_055533_create_anchors_table.php
+++ database/migrations/2016_10_16_055533_create_anchors_table.php
@@ -14,16 +14,17 @@ class CreateAnchorsTable extends Migration
     public function up()
     {
         Schema::create('anchors', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('id')->primary();
+            $table->unsignedInteger('live_id');
+            $table->string('name');
             $table->string('room_name');
-            $table->string('live_user_id')->unique();
-            $table->unsignedInteger('live_id')->references('id')->on('lives');
+            $table->string('room_id');
             $table->string('url');
             $table->string('cover');
             $table->string('avatar');
             $table->unsignedInteger('online');
-            $table->string('live_category_id', 20)->references('id')->on('live_categories');
+            $table->string('category_id');
+            $table->text('live_stream');
             $table->tinyInteger('status');
         });
     }
